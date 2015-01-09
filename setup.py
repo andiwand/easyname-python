f87292e7be8cefbca79a84d8826065fea401b38b
Andreas Stefl <stefl.andreas@gmail.com>
2015-01-10 00:17:24 +0100

    rename project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="easyname.py",
+    version="0.0.1",
+    url="https://github.com/andiwand/easyname.py",
+    license="GNU Lesser General Public License",
+    author="Andreas Stefl",
+    install_requires=[],
+    author_email="stefl.andreas@gmail.com",
+    description="easyname domain api",
+    long_description="",
+    package_dir = {"": "src"},
+    packages=["easyname"],
+    platforms="any",
+)
